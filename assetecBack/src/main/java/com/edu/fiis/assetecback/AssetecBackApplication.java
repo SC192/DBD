8a59217461e48fa073686e7404126c9f8d788541
Brandon Uscuvilca <brandon.uscuvilca.n@uni.pe>
2021-12-05 10:48:31 -0500

    proyecto creado

---
+++ assetecBack/src/main/java/com/edu/fiis/assetecback/AssetecBackApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fiis.assetecback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetecBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetecBackApplication.class, args);
+    }
+
+}
