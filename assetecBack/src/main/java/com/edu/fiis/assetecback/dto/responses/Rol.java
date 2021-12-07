ccdbda7b91de1bd8b81d665442bddf6e38e335ac
Brandon Uscuvilca <brandon.uscuvilca.n@uni.pe>
2021-12-06 23:58:14 -0500

    Avance dao

---
+++ assetecBack/src/main/java/com/edu/fiis/assetecback/dto/responses/Rol.java
@@ -0,0 +1,11 @@
+package com.edu.fiis.assetecback.dto.responses;
+
+import lombok.Data;
+
+@Data
+public class Rol {
+    private String nombrePerfil;
+    private Integer cantidad;
+    private Double totalHoras;
+    private Double costoHora;
+}
