a0498236eaf01ad1c128071214b4e62fb0b453eb
Brandon Uscuvilca <brandon.uscuvilca.n@uni.pe>
2021-12-13 13:44:59 -0500

    agregado solicitudes respuestas

---
+++ assetecBack/src/main/java/com/edu/fiis/assetecback/dto/responses/SolicitudExternoRespuesta.java
@@ -0,0 +1,13 @@
+package com.edu.fiis.assetecback.dto.responses;
+
+import lombok.Data;
+
+@Data
+public class SolicitudExternoRespuesta {
+    private Integer CodSolicitud;
+    private String FechaSolicitud;
+    private String ApellidosNombres;
+    private String Ruc;
+    private String Correo;
+    private String Estado;
+}
